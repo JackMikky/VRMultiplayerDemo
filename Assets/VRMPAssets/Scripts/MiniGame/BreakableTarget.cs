ade9afc45d8ab869317028a587666555c2cde315
luo.zhao01 <luo.zhao01@persol-avct.co.jp>
2026-01-22 19:06:20 +0900

    コメント位置修正

--- Assets/VRMPAssets/Scripts/MiniGame/BreakableTarget.cs
+++ Assets/VRMPAssets/Scripts/MiniGame/BreakableTarget.cs
@@ -113,12 +113,12 @@ namespace UnityEngine.XR.Content.Interaction
             {
                 if (collision.gameObject.CompareTag(m_ColliderTag))
                 {
-                    // Call server RPC to break the target
                     collision.gameObject.TryGetComponent<CustomProjectile>(out CustomProjectile projectile);
 
                     if (projectile != null && projectile.isLocalPlayerProjectile && projectile.LaunchPlayerID == NetworkManager.LocalClientId)
                     {
                         projectile.HitTarget(pointValue, true);
+                        // Call server RPC to break the target
                         BreakServerRpc();
                     }
                 }
